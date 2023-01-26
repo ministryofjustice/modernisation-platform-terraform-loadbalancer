971702f1a5c4bd404d7c36f46a7dddf6db7080b8
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-01-26 14:13:40 +0000

    fix

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "athena_db" {
 }
 
 output "security_group" {
-  value = len(aws_security_group.lb) > 0 ? aws_security_group.lb[0] : null
+  value = length(aws_security_group.lb) > 0 ? aws_security_group.lb[0] : null
 }
 
 output "load_balancer" {
