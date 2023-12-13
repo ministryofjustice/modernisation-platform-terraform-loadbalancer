1ddf248efc64c6f5fd922b09add547ad9bf76f6a
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-13 11:57:46 +0000

    change storage descriptor

--- main.tf
+++ main.tf
@@ -362,7 +362,7 @@ resource "aws_glue_catalog_table" "lb_log_table" {
   table_type = "EXTERNAL_TABLE"
 
   storage_descriptor {
-    location = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/" : "s3://${module.s3-bucket[0].bucket.id}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/"
+    location = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/" : "s3://${module.s3-bucket[0].bucket.id}/${var.application_name}/AWSLogs/${var.account_number}/"
     input_format = "org.apache.hadoop.mapred.TextInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
     ser_de_info {
