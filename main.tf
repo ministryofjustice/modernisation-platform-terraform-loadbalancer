e7989b0acdd112865cf36f6dc6e5f1ba2a198f1f
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-19 10:55:00 +0000

    switch value back

--- main.tf
+++ main.tf
@@ -339,7 +339,7 @@ resource "aws_glue_catalog_table" "application_lb_logs" {
     "projection.day.format"        = "yyyy/MM/dd"
     "projection.day.interval"      = "1"
     "projection.day.interval.unit" = "DAYS"
-    "storage.location.template"    = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/$${day}" : "s3://${local.s3_bucket_id}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/$${day}"
+    "storage.location.template"    = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/$${day}" : "s3://${module.s3-bucket[0].bucket.id}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/$${day}"
   }
   storage_descriptor {
     location      = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/" : "s3://${module.s3-bucket[0].bucket.id}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/"
