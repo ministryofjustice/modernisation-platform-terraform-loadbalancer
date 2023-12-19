8c5f741d038876b6cdcbba0daa2756213cec29f1
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-19 10:27:39 +0000

    change table parameters

--- main.tf
+++ main.tf
@@ -332,17 +332,14 @@ resource "aws_glue_catalog_table" "application_lb_logs" {
 
   table_type = "EXTERNAL_TABLE"
 
-  partition_keys {
-    name = "year"
-    type = "string"
-  }
-  partition_keys {
-    name = "month"
-    type = "string"
-  }
-  partition_keys {
-    name = "day"
-    type = "string"
+  parameters = {
+    projection.enabled           = "true"
+    projection.day.type          = "date"
+    projection.day.range         = "2023/12/01,NOW"
+    projection.day.format        = "yyyy/MM/dd"
+    projection.day.interval      = "1"
+    projection.day.interval.unit = "DAYS"
+    storage.location.template    = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/${day}" : "s3://${module.s3-bucket[0].bucket.id}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/${day}"
   }
   storage_descriptor {
     location      = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/" : "s3://${module.s3-bucket[0].bucket.id}/${var.application_name}/AWSLogs/${var.account_number}/elasticloadbalancing/${var.region}/"
